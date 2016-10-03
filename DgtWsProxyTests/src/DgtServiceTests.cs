ccc9f2bf4dab577faa490983ad343098617fffdc
jcaubin <jcaubin@gmail.com>
2016-10-03 20:48:01 +0200

    test bajas

--- DgtWsProxyTests/src/DgtServiceTests.cs
+++ DgtWsProxyTests/src/DgtServiceTests.cs
@@ -47,5 +47,29 @@ namespace DgtWsProxy.Tests
                 }
             }
         }
+
+        [TestMethod()]
+        public void GetDgtMicrodatosTest3()
+        {
+            DgtService s = new DgtService();
+            //DgtRequest req = new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = new DateTime(2016, 09, 05) };
+            //var resp = s.GetDgtMicrodatos(req);
+
+            //Assert.AreEqual(DgtResponseState.Ok, resp.State);
+
+            //File.WriteAllText(resp.FileName, resp.FileContent);
+
+            DateTime begin = new DateTime(2016, 09, 01);
+            DateTime end = new DateTime(2016, 09, 30);
+            for (DateTime date = begin; date <= end; date = date.AddDays(1))
+            {
+                var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Bajas, FileDate = date });
+                if (r2.State == DgtResponseState.Ok)
+                {
+                    File.WriteAllText(r2.FileName, r2.FileContent);
+                    File.AppendAllText("septiembreBajasProv.txt", r2.FileContent);
+                }
+            }
+        }
     }
 }
