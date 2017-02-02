42ff2283dd3b9362fb6d3a037def0858f3ce857d
jcaubin <jcaubin@gmail.com>
2017-02-02 21:01:43 +0100

    cliente ws

---
+++ ConsoleDgtClient/src/ClientArgs.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDgtClient
+{
+    class ClientArgs
+    {
+
+        [Option('t', HelpText = "Tipo de fichero matriculas o bajas.", Required = false)]
+        public TipoFichero? TipoFichero { get; set; }
+
+        [Option('b', "begin", Required = true, HelpText = "Fecha desde.")]
+        public DateTime Begin { get; set; }
+
+        [Option('e', "end", Required = true, HelpText = "Fecha hasta.")]
+        public DateTime End { get; set; }
+
+
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText();
+            help.AddPreOptionsLine("Utilizacion: ...");
+            help.AddOptions(this);
+            return help;
+        }
+
+    }
+}
---
+++ ConsoleDgtClient/src/Program.cs
@@ -0,0 +1,95 @@
+﻿using DgtWsProxy;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDgtClient
+{
+    class Program
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var options = new ClientArgs();
+                if (CommandLine.Parser.Default.ParseArguments(args, options))
+                {
+                    switch (options.TipoFichero)
+                    {
+                        case TipoFichero.matriculas:
+                            DownloadMatriculaciones(options.Begin, options.End);
+                            break;
+                        case TipoFichero.bajas:
+                            DownloadBajas(options.Begin, options.End);
+                            break;
+                        case TipoFichero.transferencias:
+                            DownloadTransferencias(options.Begin, options.End);
+                            break;
+                        default:
+                            Console.WriteLine("Opciones no validas");
+                            break;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void DownloadMatriculaciones(DateTime begin, DateTime end)
+        {
+            DgtService s = new DgtService();
+            string filename = string.Format("export_diario_mat_{0}_{1}.txt", begin.ToString("yyyyMMdd"), end.ToString("yyyyMMdd"));
+            for (DateTime date = begin; date <= end; date = date.AddDays(1))
+            {
+                var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = date });
+                if (r2.State == DgtResponseState.Ok)
+                {
+                    File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
+                    File.AppendAllText(filename, r2.FileContent, Encoding.Default);
+                }
+            }
+        }
+
+        private static void DownloadBajas(DateTime begin, DateTime end)
+        {
+            DgtService s = new DgtService();
+            string filename = string.Format("export_diario_bajas_{0}_{1}.txt", begin.ToString("yyyyMMdd"), end.ToString("yyyyMMdd"));
+            for (DateTime date = begin; date <= end; date = date.AddDays(1))
+            {
+                var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Bajas, FileDate = date });
+                if (r2.State == DgtResponseState.Ok)
+                {
+                    File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
+                    File.AppendAllText(filename, r2.FileContent, Encoding.Default);
+                }
+            }
+        }
+
+        private static void DownloadTransferencias(DateTime begin, DateTime end)
+        {
+            DgtService s = new DgtService();
+            string filename = string.Format("export_diario_trf_{0}_{1}.txt", begin.ToString("yyyyMMdd"), end.ToString("yyyyMMdd"));
+            for (DateTime date = begin; date <= end; date = date.AddDays(1))
+            {
+                var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Transferencias, FileDate = date });
+                if (r2.State == DgtResponseState.Ok)
+                {
+                    File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
+                    File.AppendAllText(filename, r2.FileContent, Encoding.Default);
+                }
+            }
+        }
+    }
+}
