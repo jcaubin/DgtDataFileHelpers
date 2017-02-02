c9cf9f5e8aa449b9204d9e967c623d3d2e74aa13
jcaubin <jcaubin@gmail.com>
2017-02-02 20:15:52 +0100

    ajustes uso

--- DgtWsProxyTests/src/DgtServiceTests.cs
+++ DgtWsProxyTests/src/DgtServiceTests.cs
@@ -25,7 +25,7 @@ namespace DgtWsProxy.Tests
         }
 
         [TestMethod()]
-        public void GetDgtMicrodatosTest2()
+        public void GetDgtMicrodatosTestMatriculaciones2()
         {
             DgtService s = new DgtService();
             //DgtRequest req = new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = new DateTime(2016, 09, 05) };
@@ -35,21 +35,23 @@ namespace DgtWsProxy.Tests
 
             //File.WriteAllText(resp.FileName, resp.FileContent);
 
-            DateTime begin = new DateTime(2016, 09, 01);
-            DateTime end = new DateTime(2016, 09, 30);
+            DateTime begin = new DateTime(2016, 12, 01);
+            DateTime end = DateTime.Now ;
+            string filename = string.Format("MatriculacionesProv_{0}_{1}.txt", begin.ToString("yyyyMMdd"), end.ToString("yyyyMMdd"));
             for (DateTime date = begin; date <= end; date = date.AddDays(1))
             {
                 var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = date });
                 if (r2.State == DgtResponseState.Ok)
                 {
                     File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
-                    File.AppendAllText("septiembreDemo.txt", r2.FileContent, Encoding.Default);
+                    File.AppendAllText(filename, r2.FileContent, Encoding.Default);
                 }
             }
         }
 
+
         [TestMethod()]
-        public void GetDgtMicrodatosTest3()
+        public void GetDgtMicrodatosTestBajas3()
         {
             DgtService s = new DgtService();
             //DgtRequest req = new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = new DateTime(2016, 09, 05) };
@@ -59,32 +61,56 @@ namespace DgtWsProxy.Tests
 
             //File.WriteAllText(resp.FileName, resp.FileContent);
 
-            DateTime begin = new DateTime(2016, 09, 01);
-            DateTime end = new DateTime(2016, 09, 30);
+            DateTime begin = new DateTime(2016, 10, 01);
+            DateTime end = new DateTime(2016, 10, 31);
             for (DateTime date = begin; date <= end; date = date.AddDays(1))
             {
                 var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Bajas, FileDate = date });
                 if (r2.State == DgtResponseState.Ok)
                 {
                     File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
-                    File.AppendAllText("septiembreBajasProv.txt", r2.FileContent, Encoding.Default);
+                    File.AppendAllText("octubreBajasProv.txt", r2.FileContent, Encoding.Default);
                 }
             }
         }
 
         [TestMethod()]
-        public void GetDgtMicrodatosTest4()
+        public void GetDgtMicrodatosTestTransferencias4()
         {
             DgtService s = new DgtService();
-            DateTime begin = new DateTime(2016, 09, 01);
-            DateTime end = new DateTime(2016, 09, 30);
+            DateTime begin = new DateTime(2016, 10, 01);
+            DateTime end = new DateTime(2016, 10, 31);
             for (DateTime date = begin; date <= end; date = date.AddDays(1))
             {
                 var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Transferencias, FileDate = date });
                 if (r2.State == DgtResponseState.Ok)
                 {
                     File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
-                    File.AppendAllText("septiembreTransferenciasProv.txt", r2.FileContent, Encoding.Default);
+                    File.AppendAllText("octubreTransferenciasProv.txt", r2.FileContent, Encoding.Default);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void GetDgtMicrodatosTest5()
+        {
+            DgtService s = new DgtService();
+            //DgtRequest req = new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = new DateTime(2016, 09, 05) };
+            //var resp = s.GetDgtMicrodatos(req);
+
+            //Assert.AreEqual(DgtResponseState.Ok, resp.State);
+
+            //File.WriteAllText(resp.FileName, resp.FileContent);
+
+            DateTime begin = new DateTime(2016, 10, 01);
+            DateTime end = new DateTime(2016, 10, 31);
+            for (DateTime date = begin; date <= end; date = date.AddDays(1))
+            {
+                var r2 = s.GetDgtMicrodatos(new DgtRequest() { FileType = DgtSubcategory.Matriculaciones, FileDate = date });
+                if (r2.State == DgtResponseState.Ok)
+                {
+                    File.WriteAllText(r2.FileName, r2.FileContent, Encoding.Default);
+                    File.AppendAllText("octMatProv.txt", r2.FileContent, Encoding.Default);
                 }
             }
         }
